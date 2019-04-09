ea4e4199f55733ced563aca8274017622a3a4080
Jo <josefinabiscailuz@gmail.com>
2019-04-09 19:40:23 -0400

    Final Draft 1

---
+++ donate/send_mail.php
@@ -0,0 +1,85 @@
+<?php
+/*
+This first bit sets the email address that you want the form to be submitted to.
+You will need to change this value to a valid email address that you can access.
+*/
+$webmaster_email = "biscj063@newschool.edu";
+
+/*
+This bit sets the URLs of the supporting pages.
+If you change the names of any of the pages, you will need to change the values here.
+*/
+$feedback_page = "index.html";
+$error_page = "error_message.html";
+$thankyou_page = "thank_you.html";
+
+/*
+This next bit loads the form field data into variables.
+If you add a form field, you will need to add it here.
+*/
+$email_address = $_REQUEST['email_address'] ;
+$first_name = $_REQUEST['first_name'] ;
+
+$weight = $_REQUEST['weight'] ;
+$date_mail = $_REQUEST['date_mail'] ;
+$package1 = $_REQUEST['package1'] ;
+$package2 = $_REQUEST['package2'] ;
+
+
+$msg = 
+"Name: " . $first_name . "\r\n" . 
+"Email: " . $email_address . "\r\n" . 
+"Weight: " . $weight . "\r\n" .
+"Date: " . $date_mail . "\r\n" . 
+"Envelope: " . $package1 . "\r\n" . 
+"Small Box: " . $package2  ;
+
+/*
+The following function checks for email injection.
+Specifically, it checks for carriage returns - typically used by spammers to inject a CC list.
+*/
+function isInjected($str) {
+	$injections = array('(\n+)',
+	'(\r+)',
+	'(\t+)',
+	'(%0A+)',
+	'(%0D+)',
+	'(%08+)',
+	'(%09+)'
+	);
+	$inject = join('|', $injections);
+	$inject = "/$inject/i";
+	if(preg_match($inject,$str)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+// If the user tries to access this script directly, redirect them to the feedback form,
+if (!isset($_REQUEST['email_address'])) {
+header( "Location: $feedback_page" );
+}
+
+// If the form fields are empty, redirect to the error page.
+elseif (empty($first_name) || empty($email_address)) {
+header( "Location: $error_page" );
+}
+
+/* 
+If email injection is detected, redirect to the error page.
+If you add a form field, you should add it here.
+*/
+elseif ( isInjected($email_address) || isInjected($first_name)  || isInjected($weight) || isInjected($date_mail) || isInjected($package1) || isInjected($package2) ){
+header( "Location: $error_page" );
+}
+
+// If we passed all previous tests, send the email then redirect to the thank you page.
+else {
+
+	mail( "$webmaster_email", "Feedback Form Results", $msg );
+
+	header( "Location: $thankyou_page" );
+}
+?>
